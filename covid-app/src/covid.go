8837581bd54579509da8df6d2f292bcda01d1f15
ManojPandey121 <me.nose121@gmail.com>
2021-06-28 19:40:02 +0545

    added api to fetch record of single country and remove unnecessary looping

--- covid-app/src/covid.go
+++ covid-app/src/covid.go
@@ -7,31 +7,51 @@ import (
 	"net/http"
 )
 
-const URL = "https://corona.lmao.ninja/v2/countries?yesterday=&sort="
+var (
+	ALL_COUNTRY_URL = "https://corona.lmao.ninja/v2/countries?yesterday=&sort="
+	ONE_COUNTRY_URL = "https://corona.lmao.ninja/v2/countries/"
+)
 
 func GetCoviddata(countryCase string) {
-	country := []CountryRecord{}
-	response, err := http.Get(URL)
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-		return
-	}
-	defer response.Body.Close()
-	data, _ := ioutil.ReadAll(response.Body)
-	err = json.Unmarshal(data, &country)
-	if err != nil {
-		fmt.Printf("Error unmarshilling http data %+v", err)
-		return
+	allCountries := []CountryRecord{}
+	singleCountry := CountryRecord{}
+	if countryCase == "" || countryCase == "all" {
+		response, err := http.Get(ALL_COUNTRY_URL)
+		if err != nil {
+			fmt.Printf("The HTTP request failed with error %s\n", err)
+			return
+		}
+		defer response.Body.Close()
+		data, _ := ioutil.ReadAll(response.Body)
+		err = json.Unmarshal(data, &allCountries)
+		if err != nil {
+			fmt.Printf("Error unmarshilling http data %+v", err)
+			return
+		}
+		for _, covidRecord := range allCountries {
+			PrettyPrint(covidRecord)
+		}
+	} else {
+		response, err := http.Get(ONE_COUNTRY_URL + countryCase)
+		if err != nil {
+			fmt.Printf("The HTTP request failed with error %s\n", err)
+			return
+		}
+		defer response.Body.Close()
+		data, _ := ioutil.ReadAll(response.Body)
+		err = json.Unmarshal(data, &singleCountry)
+		if err != nil {
+			fmt.Printf("Error unmarshilling http data %+v", err)
+			return
+		}
+		PrettyPrint(singleCountry)
 	}
 
-	for _, covidRecord := range country {
-		PrettyPrint(covidRecord)
-	}
 }
 
 func PrettyPrint(countryRecord CountryRecord) {
-	fmt.Printf("Covid records for %s \n", countryRecord.Country)
+	fmt.Printf("------Covid records for %s ----- \n", countryRecord.Country)
 	fmt.Printf("Total cases: %d\t Cases Found Today: %d\n ", countryRecord.Cases, countryRecord.TodayCases)
 	fmt.Printf("Total Recovered: %d\t  Total Deaths: %d\n", countryRecord.Recovered, countryRecord.Deaths)
-	fmt.Printf("======>\n")
+	fmt.Printf("======\n")
 }
